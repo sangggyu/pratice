5dab82973c3c0242da9f1931739d452aac19c630
sangggyu <124053404+sangggyu@users.noreply.github.com>
2023-06-02 10:35:44 +0900

    Delete was-practice directory

--- was-practice/src/main/java/org/example/calculate/tobe/ArithmeticOperator.java
+++
@@ -1,8 +0,0 @@
-package org.example.calculate.tobe;
-
-import org.example.calculate.domain.PositiveNumber;
-
-public interface ArithmeticOperator {
-    boolean supports(String operator);
-    int calculate(final PositiveNumber operand1, final PositiveNumber operand2);
-}
